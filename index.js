fdf90db80aad3155a491137b5f05ccb081a53876
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2023-12-16 03:58:54 +0900

    chap6まで

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ import {
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+// open the database file
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 
 const app = express();
 const server = createServer(app);
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const io = new Server(server);
+const io = new Server(server,{
+  connectionStateRecovery:{}
+});
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
@@ -26,8 +45,17 @@ io.on('connection', (socket) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('chat message', async (msg) => {
+    let result;
+    try {
+      // store the message in the database
+      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
+    // include the offset with the message
+    io.emit('chat message', msg, result.lastID);
   });
 });
 
